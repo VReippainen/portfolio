372483f8e94c484931c1bbbd944a44cf35a6d72e
Ville Reippainen <ville.reippainen@taaleri.com>
2020-12-27 17:16:18 +0200

    Add links to header

--- frontend/components/sections/Header.tsx
+++ frontend/components/sections/Header.tsx
@@ -1,14 +1,23 @@
 import { ReactElement } from 'react'
 import styled from 'styled-components'
 import { NavUrl } from '../../models/NavUrl'
+import Link from 'next/link'
 
 const Nav = styled.header`
   display: flex;
   flex-direction: row;
+  justify-content: flex-end;
   width: 100%;
   border-bottom-color: gray;
   border-bottom-width: 1px;
   border-bottom: solid;
+`
+
+const NavLink = styled.a`
+  margin-left: 16px;
+  &:hover {
+    opacity: 0.5;
+  }
 `
 
 interface Props {
@@ -18,10 +27,10 @@ interface Props {
 export function Header({ navUrls }: Props): ReactElement {
   return (
     <Nav id="nav">
-      {navUrls.map(({ name }) => (
-        <p key={name} style={{ marginLeft: '16px' }}>
-          {name}
-        </p>
+      {navUrls.map(({ name, url }) => (
+        <Link key={name} href={url} passHref>
+          <NavLink>{name}</NavLink>
+        </Link>
       ))}
     </Nav>
   )
