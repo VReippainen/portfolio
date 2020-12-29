923116e82b003c0ced0f92481fac0d9ced14451f
Ville Reippainen <ville.reippainen@taaleri.com>
2020-12-29 16:02:41 +0200

    add margin to section item on wide screen

--- frontend/components/ui/SectionContainer.tsx
+++ frontend/components/ui/SectionContainer.tsx
@@ -5,6 +5,10 @@ import { Spacings } from '../../constants/Spacings'
 const SectionItem = styled.div`
   flex: 1;
   margin-top: ${Spacings.S20}px;
+  margin-left: ${Spacings.S20}px;
+  ${MobileScreen} {
+    margin: 20px 0;
+  }
 `
 
 const SectionContainer = styled.section`
