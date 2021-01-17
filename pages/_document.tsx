6bd310fa7dd891118438ef3eb741ed489f672953
Ville Reippainen <ville.reippainen@taaleri.com>
2021-01-17 13:35:50 +0200

    add lang tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,12 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 /*
@@ -34,5 +42,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
