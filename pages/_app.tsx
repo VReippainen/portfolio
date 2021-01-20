ea2c66ffa72a582271a0827df6ebeac5c3cd5983
Ville Reippainen <ville.reippainen@taaleri.com>
2021-01-20 20:55:14 +0200

    add support for tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect } from 'react'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+}
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
   return <Component {...pageProps} />
 }
