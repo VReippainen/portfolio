0f7fbdc055bffc30a6afa283c78ba11e728399e9
Ville Reippainen <ville.reippainen@gmail.com>
2024-03-07 11:21:22 +0200

    Fix Contentful preview

--- services/contentfulService.ts
+++ services/contentfulService.ts
@@ -4,8 +4,9 @@ import { WebSiteModel } from '../models/WebSiteModel'
 const WEBSITE_ID = process.env.WEBSITE_ID ?? ''
 
 const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ?? '',
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ?? '',
+  space: process.env.CONTENTFUL_SPACE_ID ?? '',
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
+  host: process.env.CONTENTFUL_HOST ?? '',
 })
 
 export async function fetchWebsite(): Promise<Entry<WebSiteModel>> {
